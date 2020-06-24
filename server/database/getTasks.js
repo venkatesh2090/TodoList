5817225b019b9aa68865251522d48dbee0b2c94f
Venkatesh Kannan <zhac358@live.rhul.ac.uk>
2020-06-24 19:16:33 +0100

    refined exports

--- server/database/getTasks.js
+++ server/database/getTasks.js
@@ -28,30 +28,22 @@ const config = {
 
 const db = pgp(config);
 
-async function getAllTasks(userId) {
+export async function getAllTasks(userId) {
   return await db.any(`SELECT * FROM ${tableName} WHERE user_id = $1 ORDER BY id ASC`, [userId]);
 }
 
-function insertTask(task, userId) {
+export function insertTask(task, userId) {
   return db.none(`INSERT INTO ${tableName} (todo, user_id) VALUES ($1, $2)`, [task, userId]);
 }
 
-async function deleteTask(id) {
+export async function deleteTask(id) {
   await db.none(`DELETE FROM ${tableName} WHERE id = $1`, [id]);
 }
 
-async function taskDone(id) {
+export async function taskDone(id) {
   await db.none(`UPDATE ${tableName} t SET is_done=TRUE WHERE t.id = $1`, [id]);
 }
 
-async function deleteDone(userId) {
+export async function deleteDone(userId) {
 	await db.none(`DELETE FROM ${tableName} WHERE is_done AND user_id = $1`, [userId]);
 }
-
-module.exports = {
-  getAllTasks,
-  insertTask,
-  deleteTask,
-  taskDone,
-	deleteDone
-};
