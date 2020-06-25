070a3faac485d17f4d069bd76cc72ebb0fb7ac88
Venkatesh Kannan <zhac358@live.rhul.ac.uk>
2020-06-25 13:37:11 +0100

    added apply-loader options and reindented

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,54 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const pug = require('pug');
 module.exports = {
-	mode: 'development',
-	entry: {
-		app: './public/script.js'
+  mode: 'development',
+  entry: {
+    app: './public/script.js'
   },
   output: {
-		filename: '[name].bundle.js',
-		path: path.resolve(__dirname, 'dist')
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
   },
   devtool: 'inline-source-map',
   module: {
-	rules: 
-		[
-			{
-				test: /\.pug$/,
-				use: [ 
-					'pug-loader',
-					'apply-loader'
-				]
-			},
-			{
-				test: /\.css$/,
-				use: [
-					'style-loader',
-					'css-loader'
-				]
-			}
-		]
+    rules:
+      [
+        {
+          test: /\.pug$/,
+          use: [
+            {
+              loader: 'apply-loader',
+              options: {
+                obj: {
+				  title: 'Pug',
+				  users: [
+					'venkatesh',
+					'harish'
+				  ]
+                }
+              }
+            },
+            {
+              loader: 'pug-loader'
+            }
+          ]
+        },
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            'css-loader'
+          ]
+        }
+      ]
   },
   plugins: [
-		new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
-		new HtmlWebpackPlugin({
-			title: 'Pug'
-		})
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    new HtmlWebpackPlugin({
+      template: './public/test.pug',
+      filename: 'index.html'
+    })
   ]
 };
