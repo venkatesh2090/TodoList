d62ee17321aedf58f46fa9c2dcf785e01227c7ca
Venkatesh Kannan <zhac358@live.rhul.ac.uk>
2020-06-27 10:47:32 +0100

    added all static files to static route

--- server/app.js
+++ server/app.js
@@ -31,6 +31,7 @@ app.use(cookieSession({
   keys: Keygrip(['mySecret'], 'sha256'),
 }));
 
+app.use('/static', express.static(path.join(__dirname, '../public')));
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.use(function (req, res, next) {
