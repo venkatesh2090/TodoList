bb90c6cd7f7e9a9bd1c749e9a5f8d4b01498d1f1
Venkatesh Kannan <zhac358@live.rhul.ac.uk>
2020-06-27 13:17:58 +0100

    reindented with tabs

--- server/routes/signup.js
+++ server/routes/signup.js
@@ -3,27 +3,27 @@ const router = express.Router();
 import { userExists, emailExists, insertUser } from '../database/getTasks';
 
 router.get('/', function (req, res, next) {
-  res.render('signup', {
-    title: 'Sign Up'
-  });
+	res.render('signup', {
+		title: 'Sign Up'
+	});
 });
 
 router.post('/', async function (req, res, next) {
-  const isUser = await userExists(req.body.username).then(res => res.exists);
-  const isEmail = await emailExists(req.body.email).then(res => res.exists);
+	const isUser = await userExists(req.body.username).then(res => res.exists);
+	const isEmail = await emailExists(req.body.email).then(res => res.exists);
 
-  console.log(`${isUser} ${isEmail}`);
+	console.log(`${isUser} ${isEmail}`);
 
-  if (!isUser && !isEmail) {
-	console.log(`${req.body.username} ${req.body.password} ${req.body.email}`);
-	await insertUser(req.body.username, req.body.password, req.body.email);
-    res.redirect('/login');
-  } else {
-    res.render('signup', {
-	  title: 'Signup',
-	  logout: false
-    });
-  }
+	if (!isUser && !isEmail) {
+		console.log(`${req.body.username} ${req.body.password} ${req.body.email}`);
+		await insertUser(req.body.username, req.body.password, req.body.email);
+		res.redirect('/login');
+	} else {
+		res.render('signup', {
+			title: 'Signup',
+			logout: false
+		});
+	}
 });
 
 router.post('/', async function (req, res, next) {
