9e5f1100bfc4f2db3150456aa0c6f71ccac11d98
Venkatesh Kannan <zhac358@live.rhul.ac.uk>
2020-06-29 00:10:56 +0100

    fixed display none bug

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -2,4 +2,4 @@ document.getElementById('signup').onclick = function (event) {
 	window.location = '/signup';
 }
 
-document.getElementById('side-menu-button').style.display = none;
+document.getElementById('side-menu-button').style.display = 'none';
