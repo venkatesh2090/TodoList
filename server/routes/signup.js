a90c3d9d4061b8882efbe42f47681d4521835e3e
Venkatesh Kannan <zhac358@live.rhul.ac.uk>
2020-07-02 01:36:40 +0100

    password encryption

--- server/routes/signup.js
+++ server/routes/signup.js
@@ -1,6 +1,10 @@
 import express from 'express';
 const router = express.Router();
 import { userExists, emailExists, insertUser, getUserFromUsername, insertTodoGroup } from '../database/getTasks';
+import createError from 'http-errors';
+
+import bcrypt from 'bcrypt';
+const saltRounds = 10;
 
 router.get('/', function (req, res, next) {
 	res.render('signup', {
@@ -13,12 +17,17 @@ router.post('/', async function (req, res, next) {
 	const isEmail = await emailExists(req.body.email).then(res => res.exists);
 
 	if (!isUser && !isEmail) {
-		await insertUser(req.body.username, req.body.password, req.body.email);
-		const userId = await getUserFromUsername(req.body.username).then(res => res.id);
-		req.session.user = userId;
+		await bcrypt.hash(req.body.password, saltRounds).then(async hash => {
+			const userId = await insertUser(req.body.username, hash, req.body.email);
+			if (userId != -1) {
+				req.session.user = userId;
 
-		await insertTodoGroup(userId, 'default');
-		res.redirect('/');
+				await insertTodoGroup(userId, 'default');
+				res.redirect('/');
+			} else {
+				next(createError(500));
+			}
+		});
 	} else {
 		res.render('signup', {
 			title: 'Signup',
