3a3d234e6fb5723dab52cc25ff91799bbeb8ed9e
Venkatesh Kannan <zhac358@live.rhul.ac.uk>
2020-07-02 13:21:01 +0100

    returing 511 on transaction error

--- server/routes/todo-api.js
+++ server/routes/todo-api.js
@@ -11,8 +11,12 @@ router.use('/get', getRouter);
 router.use('/delete', deleteRouter);
 
 router.get('/done/:id', async function (req, res, next) {
-	await taskDone(req.params.id);
-	res.status(202).send();
+	const userId = req.session.user;
+	if (await taskDone(userId, req.params.id)) {
+		res.status(202).send();
+	} else {
+		res.status(511).send();
+	}
 });
 
 router.get('/deleteDone', async function (req, res, next) {
