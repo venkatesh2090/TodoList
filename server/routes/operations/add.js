04fd54e821ed349e4ff405c8383294eb47ca20d7
Venkatesh Kannan <zhac358@live.rhul.ac.uk>
2020-07-05 22:36:27 +0100

    added constraints; check for blank

--- server/routes/operations/add.js
+++ server/routes/operations/add.js
@@ -3,10 +3,14 @@ const router = express.Router();
 
 import { insertTodoGroup, insertTask } from '../../database/getTasks';
 
+const blank = /^ *$/;
+
 router.post('/task', async function (req, res, next) {
 	const userId = req.session.user;
-	if (userId != null || userId != undefined) {
-		await insertTask(req.body.task, userId)
+	const task = req.body.task;
+
+	if ((userId != null || userId != undefined) || (task != null || task != undefined || !blank.test(task))) {
+		await insertTask(task, userId)
 			.catch(err => console.log(err));
 		res.status(202).send();
 	} else {
@@ -18,7 +22,7 @@ router.post('/group', async function (req, res, next) {
 	const userId = req.session.user;
 	const groupName = req.body.group;
 
-	if ((userId != null || userId != undefined) && (groupName != null || groupName != undefined)) {
+	if ((userId != null || userId != undefined) && (groupName != null || groupName != undefined || !blank.test(groupName))) {
 		const id = await insertTodoGroup(userId, groupName);
 		if (id != -1) {
 			const data = {
