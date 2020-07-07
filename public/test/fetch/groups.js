6e6db735b6749fb59f5ee6386c4e1e5f8317d34e
Venkatesh Kannan <zhac358@live.rhul.ac.uk>
2020-07-07 16:26:48 +0100

    fetch groups from api

---
+++ public/test/fetch/groups.js
@@ -0,0 +1,14 @@
+export async function getGroupsList() {
+	const url = `/api/get/groups`;
+	const req = new Request(url, {
+		method: 'GET'
+	});
+
+	const res = await fetch(req);
+
+	if (res.ok) {
+		return res.json();
+	} else {
+		return false;
+	}
+}
