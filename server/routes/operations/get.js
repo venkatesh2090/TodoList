0e1bf49bdc244ccd8f00a2be354a40767f35431b
Venkatesh Kannan <zhac358@live.rhul.ac.uk>
2020-07-07 16:26:32 +0100

    api route to get groups from user id

--- server/routes/operations/get.js
+++ server/routes/operations/get.js
@@ -1,7 +1,7 @@
 import express from 'express';
 const router = express.Router();
 
-import { getTaskByGroupId } from '../../database/getTasks.js';
+import { getTaskByGroupId, getGroupsFromUserId } from '../../database/getTasks.js';
 
 router.get('/tasks/:gid', async function(req, res, next) {
 	const groupId = req.params.gid;
@@ -13,9 +13,22 @@ router.get('/tasks/:gid', async function(req, res, next) {
 	} catch (err) {
 		console.error(err);
 		res.sendStatus(511);
+	}
 });
 
 router.get('/taskall', function(req, res, next) {
+});
+
+router.get('/groups', async function(req, res, next) {
+	const userId = req.session.user;
+
+	try {
+		const groups = await getGroupsFromUserId(userId);
+		res.json(groups);
+	} catch (err) {
+		console.error(err);
+		res.sendStatus(511);
+	}
 });
 
 export default router;
