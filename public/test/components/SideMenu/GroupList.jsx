5e358ff6be59b3806c4beb2bfe3682e884d3d84f
Venkatesh Kannan <zhac358@live.rhul.ac.uk>
2020-07-07 16:27:26 +0100

    set groups list

--- public/test/components/SideMenu/GroupList.jsx
+++ public/test/components/SideMenu/GroupList.jsx
@@ -1,19 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { getGroupsList } from '../../fetch/groups';
 
-function GroupList(props) {
-	// TODO: get group List
-	/**
-	 * default from sessionStorage for now
-	 */
-	let [active, setActive] = useState(window.sessionStorage.getItem('groupId'));
-	return (
-		<div id = 'groups-container' className = 'd-flex container flex-row'>
-			<div className = 'd-flex flex-row justify-content-between align-items-center w-100'>
-				<p className = 'mb-0'> default </p>
-				<input type = 'button' className = 'btn btn-danger' value = 'X' />
+class GroupList extends React.Component {
+	constructor(props) {
+		super(props);
+		// TODO: implement get default group
+		this.state = {
+			active: window.sessionStorage.getItem('groupId'),
+			groups: []
+		}
+	}
+
+	async componentDidMount() {
+		const groups = await getGroupsList();
+		this.setState({ groups: groups != false ? groups : {}});
+	}
+
+	render() {
+		const groups = this.state.groups;
+		return (
+			<div id = 'groups-container' className = 'd-flex container flex-column'>
+				{
+					groups.map(e => {
+						return (
+							<div key = {e.id} className = {`d-flex flex-row justify-content-between align-items-center w-100 ${this.state.active == e.id ? 'active' : ''}`}>
+								<p className = 'mb-0'> {e.group_name} </p>
+								<input type = 'button' className = 'btn btn-danger' value = 'X' />
+							</div>
+						);
+					})
+				}
 			</div>
-		</div>
-	);
+		);
+	}
 }
 
 export default GroupList;
