b529c058fb4c9fb37c54c15c7282535bf97725e0
Venkatesh Kannan <zhac358@live.rhul.ac.uk>
2020-07-11 00:27:29 +0100

    created prettier lists

--- public/test/components/TODOList/TaskList.jsx
+++ public/test/components/TODOList/TaskList.jsx
@@ -8,18 +8,20 @@ class TaskList extends React.Component {
 	render() {
 		return (
 			<div className = 'd-flex flex-column w-75 align-items-start'>
-				<h1> The List </h1>
-				<ul>
+				<h1 className = 'align-self-end'> Remove impl </h1>
 				{
 					this.props.tasks.map(function(ele) {
 						return (
-							<li key = {ele.id}>
-								{ele.task}
-							</li>
+							<div key = {ele.id} className = 'w-100 d-flex flex-row align-items-center justify-content-between ongoing'>
+								<h4>{ele.task}</h4>
+								<img src = '/static/images/tick.svg' width = '30' height = '30' onClick = {function(event) {
+									// TODO: remove task 
+									console.log(`impl remove ${ele.id}`);
+								}}/>
+							</div>
 						);
 					})
 				}
-				</ul>
 			</div>
 		);
 	}
