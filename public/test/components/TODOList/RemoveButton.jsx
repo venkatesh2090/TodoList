948c35e47c7b4d51255a5cde41bf5f85076300ee
Venkatesh Kannan <zhac358@live.rhul.ac.uk>
2020-07-11 17:18:02 +0100

    added remove button for mobile

--- public/test/components/TODOList/RemoveButton.jsx
+++ public/test/components/TODOList/RemoveButton.jsx
@@ -7,8 +7,9 @@ class RemoveButton extends React.Component {
 
 	render() {
 		return (
-			<div className = 'align-self-end'>
-				<div onClick = {this.props.removeDone} className = 'remove'><div /></div>
+			<div className = 'w-100 d-flex flex-column'>
+				<input type = 'button' value = 'Remove' className = 'remove btn btn-danger d-block d-sm-none' onClick = {this.props.removeDone}/>
+				<div onClick = {this.props.removeDone} className = 'remove align-self-end d-sm-flex d-none flex-row align-items-center'><div /></div>
 			</div>
 		);
 	}
