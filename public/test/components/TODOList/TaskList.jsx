2ac4e91777159442090623be220846cbb32b037f
Venkatesh Kannan <zhac358@live.rhul.ac.uk>
2020-07-11 14:38:53 +0100

    added padding for consistency

--- public/test/components/TODOList/TaskList.jsx
+++ public/test/components/TODOList/TaskList.jsx
@@ -8,7 +8,7 @@ class TaskList extends React.Component {
 
 	render() {
 		return (
-			<div className = 'd-flex flex-column w-100'>
+			<div className = 'd-flex flex-column w-100 pt-3'>
 				{
 					this.props.tasks.map(function(ele) {
 						return (
