0f2ad99a46d9177512eb3a0c34e4af8048995c3d
Venkatesh Kannan <zhac358@live.rhul.ac.uk>
2020-07-18 00:32:34 +0100

    added try catch for server error

--- server/routes/login.js
+++ server/routes/login.js
@@ -14,31 +14,36 @@ router.get('/', function (req, res, next) {
 router.post('/', async function (req, res, next) {
 	const username = req.body.user;
 
-	let queryRes = await userExists(username);
+	try {
+		let exists = await userExists(username);
 
-	if (queryRes.exists) {
-		queryRes = await getUserFromUsername(username);
-		const hash = queryRes.password;
-		await bcrypt.compare(req.body.password, hash).then(isEqual => {
-			if (isEqual) {
-				req.session.user = queryRes.id;
-				res.redirect('/');
-			}
-			else {
-				res.render('login', {
-					title: 'Login',
-					failed: true,
-					logout: false,
-					notExists: false
-				});
-			}
-		});
-	} else {
-		res.render('login', {
-			title: 'Login',
-			failed: false,
-			notExists: true
-		});
+		if (exists) {
+			let queryRes = await getUserFromUsername(username)
+			const hash = queryRes.password;
+			await bcrypt.compare(req.body.password, hash).then(isEqual => {
+				if (isEqual) {
+					req.session.user = queryRes.id;
+					res.redirect('/');
+				}
+				else {
+					res.render('login', {
+						title: 'Login',
+						failed: true,
+						logout: false,
+						notExists: false
+					});
+				}
+			});
+		} else {
+			res.render('login', {
+				title: 'Login',
+				failed: false,
+				notExists: true
+			});
+		}
+	} catch (err) {
+		console.error(err);
+		next(500);
 	}
 });
 
